3cc41b1d8cd7b5a757ea4354b0b89c661a9f1e99
Medha Karthik <143346227+mkartuncc@users.noreply.github.com>
2023-10-27 14:18:40 -0400

    Add files via upload

--- TopBar.jsx
+++ TopBar.jsx
@@ -3,7 +3,7 @@ import {
     AppBar, Toolbar, Typography
 } from '@mui/material';
 import './TopBar.css';
-import fetchModel from "../../lib/fetchModelData";
+import axios from 'axios';
 
 /**
  * Define TopBar, a React componment of project #5
@@ -23,7 +23,7 @@ class TopBar extends React.Component {
     handleAppInfoChange(){
         const app_info = this.state.app_info;
         if (app_info === undefined){
-            fetchModel("/test/info")
+            axios.get("/test/info")
                 .then((response) =>
                 {
                     this.setState({
@@ -39,7 +39,7 @@ class TopBar extends React.Component {
         <Toolbar>
             <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>Todd Dobbs</Typography>
             <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} color="inherit">{this.props.main_content}</Typography>
-            <Typography variant="h5" component="div" color="inherit">Version: {this.state.app_info.__v}</Typography>
+            <Typography variant="h5" component="div" color="inherit">Version: {this.state.app_info.version}</Typography>
         </Toolbar>
       </AppBar>
     ) : (
